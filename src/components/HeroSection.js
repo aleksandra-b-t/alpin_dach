d4699776ef33a715b0b443eab9d350c95f9803e7
aleksandra-b-t <a.bystranowska.takahashi@gmail.com>
2020-12-07 15:34:11 -0500

    css for HeroSection

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -3,6 +3,7 @@ import '../App.css';
 import { Button } from './Button';
 import './HeroSection.css'
 
+
 function HeroSection() {
     return (
         <div className='hero-container'>
@@ -12,6 +13,9 @@ function HeroSection() {
             <div className="hero-btns">
                 <Button className='btns' buttonStyle='btn--outline' buttonSize='btn--large'>
                     Get Started
+                </Button>
+                <Button className='btns' buttonStyle='btn--primary' buttonSize='btn--large'>
+                    WATCH TRAILER <i className='far fa-play-circle' />
                 </Button>
             </div>
         </div>
