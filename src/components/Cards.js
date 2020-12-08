0cb5bd14d3390c4189838502ac882d805e752a8b
aleksandra-b-t <a.bystranowska.takahashi@gmail.com>
2020-12-08 15:55:59 -0500

    creating card and cardItem js

---
+++ src/components/Cards.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Cards() {
+    return (
+        <div className='cards'>
+            <h1>Check out these EPIC Destinations!</h1>
+            <div className='cards_container'>
+                <div className="cards_wrapper">
+                    <ul className="cards_items">
+                        
+                    </ul>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Cards
