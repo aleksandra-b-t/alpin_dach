8b9d442ba1cb387b28703fc22bd39b84a49d9dbc
aleksandra-b-t <a.bystranowska.takahashi@gmail.com>
2020-12-10 09:57:32 -0500

    adding products/services/sign-up pages

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -8,23 +8,38 @@ function Cards() {
             <h1>Check out these EPIC Destinations!</h1>
             <div className='cards_container'>
                 <div className="cards_wrapper">
-                    <ul className="cards_items">
+                    <ul className='cards__items'>
                         <CardItem 
-                        src='image/img-9.jpg'
+                        src='image/img-3.jpg'
                         text='Jakis sobie text'
                         label='adventure'
                         path='/services'/>
                         <CardItem 
-                        src='image/img-9.jpg'
-                        text='Jakis sobie text'
-                        label='adventure'
-                        path='/services'/>
-                        <CardItem 
-                        src='image/img-9.jpg'
-                        text='Jakis sobie text'
-                        label='adventure'
+                        src='image/img-3.jpg'
+                        text='happy'
+                        label='happy'
                         path='/services'/>
                     </ul>
+                    <ul className='cards__items'>
+            <CardItem
+              src='images/img-3.jpg'
+              text='Set Sail in the Atlantic Ocean visiting Uncharted Waters'
+              label='Mystery'
+              path='/services'
+            />
+            <CardItem
+              src='images/img-3.jpg'
+              text='Experience Football on Top of the Himilayan Mountains'
+              label='Adventure'
+              path='/products'
+            />
+            <CardItem
+              src='images/img-3.jpg'
+              text='Ride through the Sahara Desert on a guided camel tour'
+              label='Adrenaline'
+              path='/sign-up'
+            />
+          </ul>
                 </div>
             </div>
         </div>
